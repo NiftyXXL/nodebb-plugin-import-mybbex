69b7864d98e7c6a263a84a9147827c353b21d179
xc <td@td.localdomain>
2014-11-21 07:51:31 +0100

    quotes

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ var logPrefix = '[nodebb-plugin-import-mybbex]';
             + prefix + 'forums.name as _name, '
             + prefix + 'forums.description as _description, '
             + prefix + 'forums.disporder as _order '
-            + prefix + 'substring_index(substring_index(concat(',',forums.parentlist),',',-2),',',1) as _parentCid'
+            + 'substring_index(substring_index(concat(\',\','+ prefix +'forums.parentlist),\',\',-2),\',\',1) as _parentCid '
             + 'FROM ' + prefix + 'forums ' // filter added later
             + (start >= 0 && limit >= 0 ? 'LIMIT ' + start + ',' + limit : '');
 
